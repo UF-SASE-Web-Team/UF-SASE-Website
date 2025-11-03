abf0321500a1dc2a8748dee890c24867aa4dbce0
Alice Jiang <alicezzjiang@gmail.com>
2025-11-03 15:57:52 -0500

    25-26 calendar updated

--- src/client/components/EventsCalendar.tsx
+++ src/client/components/EventsCalendar.tsx
@@ -4,7 +4,7 @@ const EventsCalendar = () => (
     <div className="pointer-events-none absolute left-3 top-3 z-0 h-full w-full rounded-xl bg-gradient-to-b from-saseBlue to-saseGreen opacity-0 transition-opacity duration-700 ease-in-out group-hover:opacity-100"></div>
 
     <iframe
-      src="https://calendar.google.com/calendar/embed?src=47b916a8ac315c88c4dc86bb240cbd1bf1bfdae1800445aa534d7fd1818badcf%40group.calendar.google.com&ctz=America%2FNew_York"
+      src="https://calendar.google.com/calendar/embed?src=37ac4d5540136c7524b9a64daa11762754c52afa770f3f12e1ac6edca7cb59a3%40group.calendar.google.com&ctz=America%2FNew_York"
       style={{
         border: "2px solid black",
         borderRadius: "15px",

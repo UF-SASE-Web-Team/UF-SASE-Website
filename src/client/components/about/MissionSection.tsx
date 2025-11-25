0fc6987ef6aef015f7e9b9b1d3324c5d1e630d8a
Stephanie Fong <fong.s@ufl.edu>
2025-11-24 20:39:29 -0500

    refactored mission cards and carousel on home page

--- src/client/components/about/MissionSection.tsx
+++ src/client/components/about/MissionSection.tsx
@@ -9,33 +9,9 @@ const MissionSection: React.FC = () => {
       <p className={cn("mb-8 text-center text-3xl")}>To create a welcoming community where members:</p>
 
       <div className={cn("mx-auto grid max-w-7xl grid-cols-3 items-stretch gap-10 px-6")}>
-{Missions.map((s) => (
-                  <div key={s.mission} className="flex justify-center">
-                    <div
-                      className={[
-                        "relative h-full w-full",
-                        "[&>div]:h-full",
-                        "[&>div>div:nth-child(2)]:h-full",
-                        "[&>div>div:nth-child(2)>div]:h-full",
-                        // hide the background copy on desktop
-                        "[&>div>div:first-child]:hidden",
-                        // scale whole card on hover
-                        "[&>div]:transition-transform",
-                        "[&>div]:duration-300",
-                        "[&>div]:transform-gpu",
-                        "hover:[&>div]:scale-105",
-                        s.shadow === "blue"
-                          ? "hover:[&>div>div:nth-child(2)]:shadow-[12px_12px_0_#0668B3]"
-                          : "hover:[&>div>div:nth-child(2)]:shadow-[12px_12px_0_#7DC242]",
-                        // prevent inner double-scale
-                        "[&>div>div:nth-child(2)>div:hover]:scale-100",
-                      ].join(" ")}
-                    >
-                      <MissionCard image={s.image} mission="" text={s.aboutText} shadow={s.shadow} />
-                    </div>
-                  </div>
-                ))}
-
+        {Missions.map((s) => (
+          <MissionCard image={s.image} mission="" text={s.aboutText} shadow={s.shadow} />
+        ))}
       </div>
     </div>
   );

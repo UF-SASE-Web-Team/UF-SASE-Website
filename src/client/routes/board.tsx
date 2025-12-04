258364e0f9acb6c11ffb51856eac122bcb432e42
Stephanie Fong <fong.s@ufl.edu>
2025-12-03 19:43:04 -0500

    fix board on mobile

--- src/client/routes/board.tsx
+++ src/client/routes/board.tsx
@@ -62,12 +62,7 @@ export const Route = createFileRoute("/board")({
             <div className="flex justify-center">
               <div className="grid max-w-screen-lg grid-cols-1 justify-items-center gap-x-16 gap-y-5 md:grid-cols-3">
                 {section.members.map((member, idx) => (
-                  <div
-                    key={idx}
-                    id={`member-${member.name}`}
-                    className="flex aspect-square w-full max-w-[300px] cursor-pointer items-center justify-center"
-                    onClick={() => setOpenMember(member)}
-                  >
+                  <div key={idx} id={`member-${member.name}`} className="w-full max-w-[300px] cursor-pointer" onClick={() => setOpenMember(member)}>
                     <BoardMemberCard member={member} />
                   </div>
                 ))}

41d9b9a7811d775546c220552b642d6571e227de
andy! <156564624+andy-jpeg@users.noreply.github.com>
2026-01-30 23:37:55 -0500

    Add Partners Section & Adjust Chat Bubbles on Sponsors Page [FE] (#418)
    * add partners page template & shift chat bubbles down
    
    * import partner company logos
    
    * import PartnersCard & Partners mapping, adjust chat bubble margins
    
    * re-adjust logo sizes
    
    * stabilize logo aspect ratio, adjust partner card margins
    
    * remove repetitive code changes
    
    * import partner company links
    
    * fixed mispellings lol
    
    * fixed more misspellings :(
    
    * adjust padding on mobile
    
    * adjust chat bubble margin on mobile
    
    * fix whitespace gap on mobile
    
    * fix chat bubble structure & padding on mobile
    
    * refactored chat bubble for upside down tail
    
    ---------
    
    Co-authored-by: Stephanie Fong <fong.s@ufl.edu>

--- src/client/components/sponsors/ChatBubble.tsx
+++ src/client/components/sponsors/ChatBubble.tsx
@@ -1,23 +1,41 @@
 import { cn } from "@/shared/utils";
 
-export const ChatBubble = ({ children, tailSide }: { children: React.ReactNode; tailSide: "left" | "right" }) => {
+export const ChatBubble = ({ children, tailSide, upsideDown }: { children: React.ReactNode; tailSide: "left" | "right"; upsideDown?: true }) => {
   return (
     <>
+      {upsideDown ? (
+        <div
+          className={cn(
+            {
+              "left-28": tailSide == "left",
+              "right-28 ml-auto": tailSide == "right",
+            },
+            "relative top-11 h-20 w-20 -rotate-[135deg] rounded-br-lg bg-saseBlue",
+          )}
+        >
+          <div className="absolute bottom-2 right-2 h-full w-full rounded-br-lg bg-muted" />
+        </div>
+      ) : null}
+
       <div className="rounded-full bg-gradient-to-b from-saseBlue to-saseGreen p-2">
         <div className="rounded-full bg-muted p-10 font-redhat font-medium">
-          <div className="relative z-10 text-lg">{children}</div>
+          <div className="relative z-10 sm:text-2xl md:text-3xl">{children}</div>
         </div>
       </div>
 
-      {/* Message Tail */}
-      <div
-        className={cn(
-          { "left-24": tailSide == "left", "right-28 ml-auto": tailSide == "right" },
-          "relative bottom-11 h-20 w-20 rotate-45 rounded-br-lg bg-saseGreen",
-        )}
-      >
-        <div className="absolute bottom-2 right-2 h-full w-full rounded-br-lg bg-muted" />
-      </div>
+      {upsideDown ? null : (
+        <div
+          className={cn(
+            {
+              "left-28": tailSide == "left",
+              "right-28 ml-auto": tailSide == "right",
+            },
+            "relative bottom-11 h-20 w-20 rotate-45 rounded-br-lg bg-saseGreen",
+          )}
+        >
+          <div className="absolute bottom-2 right-2 h-full w-full rounded-br-lg bg-muted" />
+        </div>
+      )}
     </>
   );
 };
---
+++ src/client/information/Partners.ts
@@ -0,0 +1,54 @@
+import Citi from "@assets/Citi.jpg";
+import DraftKings from "@assets/DraftKings.png";
+import GEVernova from "@assets/GEVernova.png";
+import { imageUrls } from "@assets/imageUrls";
+import Micron from "@assets/Micron.png";
+import Solidigm from "@assets/Solidigm.png";
+import UKG from "@assets/UKG.png";
+
+const SponsorInfo = [
+  {
+    company: "Micron",
+    image: Micron,
+    shadow_color: "shadow-blue-500",
+    link: "https://www.micron.com/",
+  },
+  {
+    company: "Citi",
+    image: Citi,
+    shadow_color: "shadow-[#FFD700]",
+    link: "https://www.citi.com/",
+  },
+  {
+    company: "UKG",
+    image: UKG,
+    shadow_color: "shadow-[#CD7F32]",
+    link: "https://www.ukg.com/",
+  },
+  {
+    company: "Draft Kings",
+    image: DraftKings,
+    shadow_color: "shadow-[#CD7F32]",
+    link: "https://www.draftkings.com/",
+  },
+  {
+    company: "GE Vernova",
+    image: GEVernova,
+    shadow_color: "shadow-[#CD7F32]",
+    link: "https://www.gevernova.com/",
+  },
+  {
+    company: "Solidigm",
+    image: Solidigm,
+    shadow_color: "shadow-[#CD7F32]",
+    link: "https://www.solidigm.com/",
+  },
+  {
+    company: "Google",
+    image: imageUrls["Google.png"],
+    shadow_color: "shadow-[#CD7F32]",
+    link: "https://www.google.com/",
+  },
+];
+
+export default SponsorInfo;

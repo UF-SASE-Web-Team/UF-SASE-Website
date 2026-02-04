f8c6ff2b3803472ecab18f8fd8e85abad15ebe18
andy-jpeg <156564624+andy-jpeg@users.noreply.github.com>
2026-02-03 20:34:31 -0500

    update set/interns faq

--- src/client/routes/interns.tsx
+++ src/client/routes/interns.tsx
@@ -53,7 +53,7 @@ export const Route = createFileRoute("/interns")({
         <div className="flex w-full flex-col items-center pb-10">
           {/* Group Picture & App Status */}
           <div className="flex w-full flex-col items-center justify-center bg-black py-10 dark:bg-greenBackground">
-            <ApplicationPhoto image={imageUrls["SaseInterns.png"]} applicationStatus="CLOSED" nextSemester="Fall 2027" />
+            <ApplicationPhoto image={imageUrls["SaseInterns.png"]} applicationStatus="CLOSED" nextSemester="Fall 2026" />
           </div>
 
           {/* Testimonials */}

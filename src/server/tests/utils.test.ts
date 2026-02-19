bb3be24c953e949fa4f9a7bb88a3b7425a01ad0d
nocturne918 <andrewchuangsaladin@gmail.com>
2026-02-18 23:21:50 -0500

    Refactored from vitest to bun test runner

--- src/server/tests/utils.test.ts
+++ src/server/tests/utils.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest";
-import { emailRegex, passwordRegex } from "../../shared/utils";
+import { emailRegex, passwordRegex } from "@/shared/utils";
+import { describe, expect, it } from "bun:test";
 
 describe("Auth Regex Validators", () => {
   describe("emailRegex", () => {

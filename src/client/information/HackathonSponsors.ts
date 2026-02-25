c0619701dcb5a77c1cab31e160777b66582b8d5e
andy-jpeg <156564624+andy-jpeg@users.noreply.github.com>
2026-02-24 21:13:21 -0500

    import company images & connect page to header

---
+++ src/client/information/HackathonSponsors.ts
@@ -0,0 +1,45 @@
+import AliveCreditUnion from "@assets/AliveCreditUnion.png";
+import Celsius from "@assets/Celsius.png";
+import FCI from "@assets/FCI.png";
+import { imageUrls } from "@assets/imageUrls";
+import PureButtons from "@assets/PureButtons.png";
+
+const SponsorInfo = [
+  {
+    company: "ExxonMobil",
+    image: imageUrls["ExxonMobilLogo.png"],
+    link: "https://corporate.exxonmobil.com/",
+  },
+  {
+    company: "P&G",
+    image: imageUrls["P&GLogo.jpeg"],
+    link: "https://us.pg.com/",
+  },
+  {
+    company: "Sandia",
+    image: imageUrls["SandiaLogo.jpg"],
+    link: "https://www.sandia.gov",
+  },
+  {
+    company: "Alive Credit Union",
+    image: AliveCreditUnion,
+    link: "https://www.alivecu.coop",
+  },
+  {
+    company: "Florida Community Innovation",
+    image: FCI,
+    link: "https://www.floridainnovation.org",
+  },
+  {
+    company: "Celsius",
+    image: Celsius,
+    link: "https://www.celsius.com",
+  },
+  {
+    company: "Pure Buttons",
+    image: PureButtons,
+    link: "https://www.purebuttons.com",
+  },
+];
+
+export default SponsorInfo;
--- src/client/routes/sasehacks.tsx
+++ src/client/routes/sasehacks.tsx
@@ -1,12 +1,12 @@
+import HackathonSponsors from "@/client/information/HackathonSponsors";
 import { cn } from "@/shared/utils";
 import Calendar from "@assets/Calendar.png";
 import { imageUrls } from "@assets/imageUrls";
 import Pinpoint from "@assets/Pinpoint.png";
-import SaseHacksPoster from "@assets/SaseHacksPoster.png";
+import SASEHacksPoster from "@assets/SASEHacksPoster.png";
 import { OmbreDivider } from "@components/custom_ui/OmbreDivider";
 import { useIsMobile } from "@hooks/useIsMobile";
 import { createFileRoute } from "@tanstack/react-router";
-import { calendar } from "googleapis/build/src/apis/calendar";
 import { seo } from "../utils/seo";
 
 export const Route = createFileRoute("/sasehacks")({
@@ -34,7 +34,7 @@ export const Route = createFileRoute("/sasehacks")({
         <div className="my-6 flex flex-row text-xl font-thin sm:gap-24 md:gap-32">
           <div className="flex flex-row items-center gap-6">
             <img src={Calendar} alt="Calendar" style={{ width: "50px" }} />
-            March 7-8, 2025
+            March 7-8, 2026
           </div>
           <div className="flex flex-row items-center gap-6">
             <img src={Pinpoint} alt="Pinpoint" style={{ width: "40px" }} />
@@ -46,13 +46,13 @@ export const Route = createFileRoute("/sasehacks")({
 
         <div
           className={cn(
-            "flex gap-32 text-xl font-thin sm:my-12 md:text-xl xl:my-6",
-            isMobile ? "flex-col items-center sm:px-12" : "flex-row sm:px-12 md:px-40",
+            "max-w-8xl flex gap-16 text-xl font-thin sm:my-16 md:text-xl 2xl:text-2xl",
+            isMobile ? "flex-col items-center gap-16 sm:px-12 md:px-40" : "flex-row sm:px-12 md:px-40",
           )}
         >
           <p>
-            SASEHACKS will be a 24-hour hackathon where students from any university can learn new skills, network, attend workshops, build innovative
-            projects, and compete for exciting category prizes!
+            <span className="font-semibold text-saseBlue">SASEHACKS</span> will be a 24-hour hackathon where students from any university can learn
+            new skills, network, attend workshops, build innovative projects, and compete for exciting category prizes!
             <br></br>
             <br></br>
             Meals are provided, overnight hacking is encouraged, and all majors and skill levels are welcome. Teams can have between 1 and 4 hackers,
@@ -62,7 +62,28 @@ export const Route = createFileRoute("/sasehacks")({
             Whether you want to build alongside a team or share your knowledge with others, we have a spot for you!
           </p>
 
-          <img src={SaseHacksPoster} alt="SaseHacks promotional poster" className="sm:w-[350px] md:w-[150px]" style={{ width: "350px" }} />
+          <img
+            src={SASEHacksPoster}
+            alt="SASEHacks promotional poster"
+            className="rounded-2xl shadow-xl sm:w-[350px] md:w-[150px]"
+            style={{ width: "350px" }}
+          />
+        </div>
+
+        <p className="header-text">SPONSORS</p>
+
+        <div className="relative my-8 flex max-w-7xl flex-col items-center justify-center">
+          <div className="ombre-background rounded-2xl p-2">
+            <div className="grid w-full max-w-96 grid-cols-1 items-stretch gap-12 rounded-2xl bg-gradient-to-b from-gray-100 to-white p-10 dark:from-gray-900 dark:to-black lg:max-w-full lg:grid-cols-3 lg:p-24 xl:grid-cols-4">
+              {/* {HackathonSponsors.map((sponsor) => (
+                <PartnerCard
+                  companyName={sponsor.company}
+                  image={sponsor.image}
+                  link={sponsor.link}
+                />
+              ))} */}
+            </div>
+          </div>
         </div>
       </div>
     );

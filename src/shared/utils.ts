35c56fd61611628254c3c8b5be01159564a8d682
RJ Tabelon <tabelonrj@gmail.com>
2026-04-20 15:40:12 -0400

    fix linting issue

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,7 +2,7 @@ import type { SuccessResponse } from "@schema/responseSchema";
 import { errorResponseSchema, successResponseSchema } from "@schema/responseSchema";
 import { clsx, type ClassValue } from "clsx";
 import type { Context } from "hono";
-import type { StatusCode } from "hono/utils/http-status";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 import type React from "react";
 import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
@@ -27,7 +27,7 @@ export const createSuccessResponse = <T>(c: Context, result: T, message: string 
   return c.json({ data: result, message, meta });
 };
 
-export const createErrorResponse = (c: Context, errCode: string, errMsg: string, statusCode: StatusCode = 500) =>
+export const createErrorResponse = (c: Context, errCode: string, errMsg: string, statusCode: ContentfulStatusCode = 500) =>
   c.json({ error: { errCode, errMsg } }, statusCode);
 
 export const apiFetch = async (url: string, options: RequestInit = {}, dataSuccessSchema: ZodTypeAny): Promise<SuccessResponse> => {
